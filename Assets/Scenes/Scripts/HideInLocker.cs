4bf2baccfb3cada2886e24b87167b0a7e45439b9
VictorHernandez-prog <personalvh06@gmail.com>
2025-10-27 20:05:45 -0400

    Fortnite
    John Fortnite

--- Assets/Scenes/Scripts/HideInLocker.cs
+++ Assets/Scenes/Scripts/HideInLocker.cs
@@ -3,47 +3,62 @@ using UnityEngine;
 public class HideInLocker : MonoBehaviour
 {
     private bool isPlayerClose = false;
-    private PlayerControls Player;
+    private PlayerControls playerControls;
     public bool isPlayerHiding { get; private set; } = false;
     private Lock currentLocker;
+    private Vector3 lastPlayerPosition; // <-- NEW: store where you entered
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        Player = GetComponent<PlayerControls>();
+        playerControls = GetComponent<PlayerControls>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (isPlayerClose && Input.GetKeyDown(KeyCode.E))
         {
+            if (currentLocker == null) return;
+
             isPlayerHiding = !isPlayerHiding;
 
-            if (isPlayerHiding )
+            if (isPlayerHiding)
             {
+                // Save where player was before hiding
+                lastPlayerPosition = transform.position;
+
+                // Move into locker
                 transform.position = currentLocker.HidingSpot.position;
-                Player.EnterLocker();
+                playerControls?.EnterLocker();
+
+                Debug.Log("Player entered locker. Saved position: " + lastPlayerPosition);
             }
             else
             {
-                Player.ExitLocker();
+                // Move back to where player entered
+                transform.position = lastPlayerPosition;
+
+                playerControls?.ExitLocker();
+
+                Debug.Log("Player exited locker to saved position: " + lastPlayerPosition);
             }
         }
-    }   
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Lockers"))
+        {
             isPlayerClose = true;
-        currentLocker = other.GetComponent<Lock>();
+            currentLocker = other.GetComponentInParent<Lock>();
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Lockers"))
+        {
             isPlayerClose = false;
-        currentLocker = null;
+            currentLocker = null;
+        }
     }
-
 }
